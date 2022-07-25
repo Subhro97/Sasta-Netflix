bf843ed496413b82f32f8eddf5daf4ce1d90d6eb
Subhrojit <subhrojit.t@collegedunia.com>
2022-07-25 22:09:46 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider, ReactReduxContext } from "react-redux";
-import "./index.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider, ReactReduxContext } from 'react-redux';
+import './index.scss';
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-import App from "./App";
-import configureStore, { history } from "./store";
-import { ConnectedRouter } from "connected-react-router";
+import App from './App';
+import configureStore, { history } from './store';
+import { ConnectedRouter } from 'connected-react-router';
 
 const store = configureStore({});
 
 ReactDOM.render(
   <Provider store={store} context={ReactReduxContext}>
-    <ConnectedRouter history={history} context={ReactReduxContext}>
+    <ConnectedRouter
+      history={history}
+      context={ReactReduxContext}
+    >
       <App />
     </ConnectedRouter>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
